79afb70cbcd70340fb7bb3d1fce1bfaec55f61cf
Dennis Hell <hellpoethero1994@gmail.com>
2019-04-04 23:13:57 +0900

    abc

--- variationalLDA/LdaEstimate.py
+++ variationalLDA/LdaEstimate.py
@@ -2,39 +2,38 @@ import random
 
 
 class LdaEstimate:
-	LAG = 10
-	NUM_INIT = 1
-	EM_CONVERGED = 0
-	EM_MAX_ITER = 0
-	ESTIMATE_ALPHA = 0
-	INITIAL_ALPHA = 0
-	K = 0
+    LAG = 10
+    NUM_INIT = 1
+    EM_CONVERGED = 0
+    EM_MAX_ITER = 0
+    ESTIMATE_ALPHA = 0
+    INITIAL_ALPHA = 0
+    K = 0
 
-	@staticmethod
-	def myrand():
-		return random.random()
+    @staticmethod
+    def myrand():
+        return random.random()
 
-	@staticmethod
-	def initial_model(start, corpus, num_topics, alpha):
-		pass
+    @staticmethod
+    def initial_model(start, corpus, num_topics, alpha):
+        pass
 
-	@staticmethod
-	def doc_em(doc, gamma, model, next_model):
-		pass
+    @staticmethod
+    def doc_em(doc, gamma, model, next_model):
+        pass
 
-	@staticmethod
-	def save_gamma(filename, gamma, num_docs, num_topics):
-		pass
+    @staticmethod
+    def save_gamma(filename, gamma, num_docs, num_topics):
+        pass
 
-	@staticmethod
-	def run_em(start, directory, corpus):
-		pass
+    @staticmethod
+    def run_em(start, directory, corpus):
+        pass
 
-	@staticmethod
-	def read_settings(filename):
-		pass
+    @staticmethod
+    def read_settings(filename):
+        pass
 
-	@staticmethod
-	def infer(model_root, save, corpus):
-		pass
-
+    @staticmethod
+    def infer(model_root, save, corpus):
+        pass
